71dd50b66eda8bb973665c9f00e30bfd70b3c3e1
李烜 <271276749@qq.com>
2018-01-29 22:35:03 +0800

    1

--- QuickstartIdentityServer/Program.cs
+++ QuickstartIdentityServer/Program.cs
@@ -13,6 +13,6 @@ namespace QuickstartIdentityServer
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();
+                .Build(); 
     }
 }
