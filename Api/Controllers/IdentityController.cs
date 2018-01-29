9870b96f3f1ce0606d115b9b99a776ca2ce605be
李烜 <271276749@qq.com>
2018-01-29 22:32:25 +0800

    1

---
+++ Api/Controllers/IdentityController.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Api.Controllers
+{
+    [Route("api1/[controller]")]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+
+            return Content("1312");
+           // return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
